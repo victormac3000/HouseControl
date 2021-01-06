7a7abd20d07756beb430a1dcd239cf7be79364e2
Víctor Martínez <vmartc06@estudiantes.unileon.es>
2021-01-06 17:30:54 +0100

    Created main, edited gitignore

---
+++ src/es/victor/mcplugins/HouseControl/MainHouseControl.java
@@ -0,0 +1,9 @@
+package es.victor.mcplugins.HouseControl;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainHouseControl extends JavaPlugin implements CommandExecutor {
+
+}
